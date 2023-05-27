b9a76ca5bd0eddc6153191931240a1d308e1e85e
Luis Micu <miculuis1@gmail.com>
2023-05-28 02:32:44 +0300

    added placeholders for charts

--- src/pages/Charts/Bar.tsx
+++ src/pages/Charts/Bar.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import { Header } from '@/components';
 
 const Bar = () => {
   return (
-    <div>Bar</div>
-  )
-}
+    <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
+      <Header category='Chart' title='Inflation Rate Bar' />
+      <div className='w-full'></div>
+    </div>
+  );
+};
 
-export default Bar
+export default Bar;
--- src/pages/Charts/Stacked.tsx
+++ src/pages/Charts/Stacked.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import { Header } from '../../components';
 
 const Stacked = () => {
   return (
-    <div>Stacked</div>
-  )
-}
+    <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
+      <Header category='Chart' title='Inflation Rate Stacked' />
+      <div className='w-full'></div>
+    </div>
+  );
+};
 
-export default Stacked
+export default Stacked;
