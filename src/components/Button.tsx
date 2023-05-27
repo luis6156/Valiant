867d40c504bc9b3549cd19a5b086f7e914aea61c
Luis Micu <miculuis1@gmail.com>
2023-05-27 21:58:03 +0300

    added homepage

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
-const Button = () => {
-  return <div>Button</div>;
+interface Props {
+  bgColor: string;
+  color: string;
+  size: string;
+  text: string;
+  borderRadius: string;
+}
+
+const Button = ({ bgColor, color, size, text, borderRadius }: Props) => {
+  return (
+    <button
+      type='button'
+      className={`text-${size} p-3 hover:drop-shadow-xl`}
+      style={{ backgroundColor: bgColor, color, borderRadius }}
+    >
+      {text}
+    </button>
+  );
 };
 
 export default Button;
