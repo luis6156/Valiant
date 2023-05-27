3022a9d391991f2809495ecec174a06be60d177d
Luis Micu <miculuis1@gmail.com>
2023-05-28 00:13:33 +0300

    added data tables

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
 
-const Header = () => {
-  return (
-    <div>Header</div>
-  )
+interface Props {
+  category: string;
+  title: string;
 }
 
-export default Header
+const Header = ({ category, title }: Props) => {
+  return (
+    <div className='mb-10'>
+      <p className='text-gray-400'>{category}</p>
+      <p className='text-3xl font-extrabold tracking-tight text-slate-900'>
+        {title}
+      </p>
+    </div>
+  );
+};
+
+export default Header;
