d260478cfc9386cb21be39b8b525e59b1cbfa21a
Luis Micu <miculuis1@gmail.com>
2023-05-28 01:31:45 +0300

    added theme and color preferences

--- src/contexts/ContextProvider.tsx
+++ src/contexts/ContextProvider.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { ChangeEvent, createContext, useContext, useState } from 'react';
 
 interface ContextStateMinor {
   chat: boolean;
@@ -15,6 +15,12 @@ interface ContextState {
   handleClick: (clicked: string) => void;
   screenSize: number;
   setScreenSize: React.Dispatch<React.SetStateAction<number>>;
+  currentColor: string;
+  currentMode: string;
+  themeSettings: boolean;
+  setThemeSettings: React.Dispatch<React.SetStateAction<boolean>>;
+  setMode: (e: ChangeEvent<HTMLInputElement>) => void;
+  setColor: (mode: string) => void;
 }
 
 const StateContext = createContext<ContextState>({} as ContextState);
@@ -34,6 +40,22 @@ export const ContextProvider = ({ children }: Props) => {
   const [activeMenu, setActiveMenu] = useState(true);
   const [isClicked, setIsClicked] = useState(initialState);
   const [screenSize, setScreenSize] = useState({} as number);
+  const [currentColor, setCurrentColor] = useState('#03c9d7');
+  const [currentMode, setCurrentMode] = useState('Light');
+  const [themeSettings, setThemeSettings] = useState(false);
+
+  const setMode = (e: ChangeEvent<HTMLInputElement>) => {
+    const target = e.target as HTMLInputElement;
+    if (target) {
+      setCurrentMode(target.value);
+      localStorage.setItem('themeMode', target.value);
+    }
+  };
+
+  const setColor = (mode: string) => {
+    setCurrentColor(mode);
+    localStorage.setItem('colorMode', mode);
+  };
 
   const handleClick = (clicked: string) => {
     setIsClicked({ ...initialState, [clicked]: true });
@@ -48,7 +70,13 @@ export const ContextProvider = ({ children }: Props) => {
         setIsClicked,
         handleClick,
         screenSize,
-        setScreenSize
+        setScreenSize,
+        currentColor,
+        currentMode,
+        themeSettings,
+        setThemeSettings,
+        setMode,
+        setColor,
       }}
     >
       {children}
