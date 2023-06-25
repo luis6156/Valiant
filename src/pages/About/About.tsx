0f80111d085da8d1cdd69538d69e0c67729367e7
Luis Micu <miculuis1@gmail.com>
2023-06-25 19:12:33 +0300

    finished about page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -10,7 +10,23 @@ const About = () => {
           subtitle='Build from the ground up by the Data Pilots'
         />
       </div>
-      <p>Thank you for choosing us!</p>
+      <p className='info'>Thank you to all authors who have made the scripts we use open-source:</p>
+      <ul>
+        <li>MsDorkDump</li>
+        <li>Photon</li>
+        <li>Webenum</li>
+        <li>CrossLinked</li>
+        <li>Poastal</li>
+        <li>SocialScan</li>
+        <li>GitRekt</li>
+        <li>GitStalk</li>
+        <li>PhishMailer</li>
+      </ul>
+      <p className='info'>All of the repositories have the author and URL linked in the corresponding script page.</p>
+      <p className='info mt-3'>All icons used have been procured from Iconify.</p>
+      <p className='info mt-5'><strong>This tool is distributed under the GPL v3 license.</strong></p>
+      <p className='info mt-3'><strong>The author is not responsible for any malicious intent.</strong></p>
+      <p className='info mt-5'><strong>Copyright© Micu Florian-Luis 2023</strong></p>
     </>
   );
 };
